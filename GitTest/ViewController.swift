91f1bc11e7b90872118287dbb5a4ecc44e68f522
zrodin <mike@captureco3d.com>
2018-03-25 22:16:09 -0400

    Added a pointless variable

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let name = "SomeUserName"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
